d7a90bc734599a0579b1b2eb22cc0e647ffe163a
Julian Maierl <scomputerlp@gmail.com>
2015-04-28 16:22:43 +0200

    Initial Commit

---
+++ src/chiefparrot/communicator/SocketCommunicator.java
@@ -0,0 +1,11 @@
+package chiefparrot.communicator;
+
+/**
+ * ChiefParrotJava
+ *
+ * @author Julian Maierl
+ * @version 1.0.0
+ */
+public class SocketCommunicator extends Communicator{
+
+}
---
+++ src/chiefparrot/visualisation/controller/TrackingMapController.java
@@ -0,0 +1,11 @@
+package chiefparrot.visualisation.controller;
+
+/**
+ * ChiefParrotJava
+ *
+ * @author Julian Maierl
+ * @version 1.0.0
+ */
+public class TrackingMapController {
+    
+}
