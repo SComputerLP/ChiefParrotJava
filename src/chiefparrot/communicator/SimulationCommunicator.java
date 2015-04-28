168ba3f2ed470b1869f821e4d76097a43c684f2f
LL <lisa.loigge@gmx.at>
2015-04-28 16:27:29 +0200

    Revert "Revert "Initial Commit""
    This reverts commit 963a5a658ff66a94f72d75dde63436f1c6b3c3e7.

---
+++ src/chiefparrot/communicator/SimulationCommunicator.java
@@ -0,0 +1,11 @@
+package chiefparrot.communicator;
+
+/**
+ * ChiefParrotJava
+ *
+ * @author Julian Maierl
+ * @version 1.0.0
+ */
+public class SimulationCommunicator extends Communicator{
+    
+}
---
+++ src/chiefparrot/data/SentCommand.java
@@ -0,0 +1,11 @@
+package chiefparrot.data;
+
+/**
+ * ChiefParrotJava
+ *
+ * @author Julian Maierl
+ * @version 1.0.0
+ */
+public class SentCommand extends Message{
+
+}
