bc975bbe90a60ee8415a984f5c7915a04eef3792
Julian Maierl <scomputerlp@gmail.com>
2015-05-06 20:52:26 +0200

    Changes

--- src/chiefparrot/data/CommandEnum.java
+++ src/chiefparrot/data/CommandEnum.java
@@ -4,44 +4,39 @@ package chiefparrot.data;
  *
  * @author Julian Maierl
  */
-public enum CommandEnum
-{
-  MOVE_RELATIVE("move_relative"), MOVE_ABSOULTE("move_absolute"), MOVE_HOME("move_home");
+public enum CommandEnum {
 
-  String name;
-  
-  /**
-   * 
-   * @param name name
-   */
-  private CommandEnum(String name)
-  {
-    this.name = name;
-  }
+    MOVE_RELATIVE("move_relative"), MOVE_ABSOULTE("move_absolute"), MOVE_HOME("move_home");
 
-  /**
-   * 
-   * @return name
-   */
-  public String getName()
-  {
-    return name;
-  }
+    String name;
 
-  /**
-   * 
-   * @param s string value
-   * @return CommandEnum
-   */
-  public CommandEnum stringToEnum(String s)
-  {
-    for (CommandEnum e : CommandEnum.values())
-    {
-      if (s.equals(e.getName()))
-      {
-        return e;
-      }
+    /**
+     *
+     * @param name name
+     */
+    private CommandEnum(String name) {
+        this.name = name;
     }
-    return null;
-  }
+
+    /**
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param s string value
+     * @return CommandEnum
+     */
+    public CommandEnum stringToEnum(String s) {
+        for (CommandEnum e : CommandEnum.values()) {
+            if (s.equals(e.getName())) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
